1421549868d56ce26e70e3cf4c4dbfef4eaf134c
Afonso Bezerra <afonsobezerra199@gmail.com>
2023-06-15 14:36:20 -0300

    fix

--- src/assets/svg/facebook.tsx
+++
@@ -1,9 +0,0 @@
-const Facebook = () => {
-    return (
-        <svg width="11" height="22" viewBox="0 0 11 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M7.25283 6.60066V4.84027C7.25283 3.98162 7.86374 3.7827 8.29351 3.7827H10.9361V0.013261L7.29545 0C3.25702 0 2.3371 2.81133 2.3371 4.61151V6.60066H0V11H2.35841V22H7.07168V11H10.5702L10.7407 9.27275L11 6.60066H7.25283Z" fill="#011F2F" />
-        </svg>
-    )
-}
-
-export default Facebook
--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,5 +1,5 @@
 import Endereco from "assets/svg/endereco"
-import Facebook from "assets/svg/facebook"
+import LinkeDin from "assets/svg/linkedin"
 import Instagram from "assets/svg/instagram"
 import Tel from "assets/svg/tel"
 import Link from "next/link"
@@ -9,13 +9,13 @@ const Footer = () => {
         <div className="w-full h-fit bg-white flex justify-center items-center p-[2rem] ">
             <div className="w-[90%] h-full flex justify-between items-center max-xl:w-[95%] max-md:w-[100%] max-sm:flex-col max-sm:gap-[2rem]">
                 <div className="flex flex-col gap-[1rem] max-sm:items-center">
-                    <span className="text-[#011F2F] flex items-center gap-[1rem] max-sm:text-center max-sm:items-start"><Endereco /> Av. Ademar de Barros, 195 - Vila Trujillo, Sorocaba - SP</span>
-                    <span className="text-[#011F2F] flex items-center gap-[1rem] max-sm:text-center"><Tel /> (15) 99817-9909 • (15) 3318-1531</span>
+                    <span className="text-[#011F2F] flex items-center gap-[1rem] max-sm:text-center max-sm:items-start"><Endereco /> Rua: Waldemar Rodrigues de Barros n° 200, Estância Conceição CEP 18206-105</span>
+                    <span className="text-[#011F2F] flex items-center gap-[1rem] max-sm:text-center"><Tel /> (15) (15) 99817-9909 • (15) 3318-1531</span>
                 </div>
                 <div className="flex flex-col gap-[1rem] items-end max-sm:items-center">
-                    <div className="flex gap-[1rem]">
-                        <Link href={'/'} className="transition-all hover:scale-[1.05]"><Facebook /></Link>
-                        <Link href={'/'} className="transition-all hover:scale-[1.05]"><Instagram /></Link>
+                    <div className="flex gap-[1rem] items-center">
+                        <Link href="https://www.linkedin.com/company/deal-empreendimentos/" className="transition-all hover:scale-[1.05]"><LinkeDin /></Link>
+                        <Link href="https://www.instagram.com/dealempreendimentos/" className="transition-all hover:scale-[1.05]"><Instagram /></Link>
                     </div>
                     <Link href={'/'} className="text-[#011F2F]">Intelligence by Core.ag</Link>
                 </div>
